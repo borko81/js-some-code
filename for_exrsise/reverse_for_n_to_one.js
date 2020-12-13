c028806b24d400b4a142eadf0d7e2bb2f28651ef
Borko <Borko@abv.bg>
2020-12-13 20:10:24 +0200

    Change prevoius to log only even

--- for_exrsise/reverse_for_n_to_one.js
+++ for_exrsise/reverse_for_n_to_one.js
@@ -3,9 +3,11 @@ function solve(e, step=1) {
     let line_row = 0
     for (i=end; i>=0; i-=step) {
         line_row += 1
-        console.log(`${line_row}: ${i}`);
+        if (i % 2 === 0){
+            console.log(`${line_row}: ${i}`);
+        }
     }
 }
 
 
-solve("5", 2)
+solve("5")
