6a1278a6605af07b87727791f7c8c8506238b6f5
Borko <bstoilov81@gmail.com>
2020-12-23 20:00:25 +0200

    Add closure

---
+++ wish_i_know/closure.js
@@ -0,0 +1,24 @@
+const someClosure = () => {
+	let privateMessage = 'this is a private';
+
+	return {
+		//geter
+		accessmessage: () => console.log(privateMessage),
+		//setter
+		changemessage: (value) => privateMessage = value
+	}
+}
+
+const test = someClosure()
+test.changemessage('Borko')
+test.accessmessage()
+
+//increment value
+const addNumbers = (value) => {
+	return (nextvalue) => {
+		return value + nextvalue;
+	}
+}
+
+const num = addNumbers(10)
+console.log(num(10))
