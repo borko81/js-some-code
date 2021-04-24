3db881a7ffafe4ffa8c3436b385028f029aa6c99
Borko <bstoilov81@gmail.com>
2021-04-24 18:33:31 +0300

    Dom color

--- with_html/one/js.js
+++ with_html/one/js.js
@@ -7,13 +7,20 @@ function myFunction() {
 function addSomething(a, b) {
     let result = a + b;
     document.getElementById('test').innerHTML = result;
-    alert(result)
+}
+
+function changeColorBody() {
+    document.body.style.background = 'yellow'
 }
 
 let button = document.getElementById('but')
+let color_button = document.getElementById("changecolor")
+
 
 button.addEventListener("click", function (){
     let choice_one = document.forms['foo'].first.value
     let choice_two = document.forms['foo'].last.value
     addSomething(choice_one, choice_two)
 });
+
+color_button.addEventListener("click", changeColorBody)
