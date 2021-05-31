8d803079ad87c3927322fe8d213dd8bf7728aca3
Borko <bstoilov81@gmail.com>
2021-05-31 21:08:20 +0300

    Add add adn remove element from array

---
+++ arrays/add_and_remove_elements.js
@@ -0,0 +1,23 @@
+function solve(elemnts) {
+    let result = [];
+    for (i=0;i<elemnts.length; i++) {
+        if (elemnts[i] == 'add') {
+            result.push(i+1)
+        } else if (elemnts[i] == 'remove') {
+            if (result.length == 0) {
+                result.push('Empty')
+            }
+            else {
+                result.pop()
+            }
+        }
+    }
+    return result.join('\n')
+}
+
+
+let result = solve(['remove', 
+'remove', 
+'remove'])
+
+console.log(result);
