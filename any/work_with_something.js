d51b64b5db37dca22f98b20938c1382cde4a476c
Borko <bstoilov81@gmail.com>
2021-08-20 22:55:55 +0300

    testing something

---
+++ any/work_with_something.js
@@ -0,0 +1,28 @@
+// let name = 'borko'
+// console.log(name[0])
+
+// // Copy array
+
+// let arr_one = ['a', 'b', 'c']
+// let arr_two = [...arr_one]
+// arr_one.push('d')
+// console.log(arr_one)
+// console.log(arr_two)
+
+// Turn number to Binary
+
+// let i = 16;
+// console.log(i.toString(2))
+
+// Divide number
+
+// console.log(27 >> 1)
+// console.log(26 % 13)
+// console.log(26 % 13 === 0)
+// console.log(27 & 1 === 1)
+
+// Ternar operator
+
+// let a = 3;
+// (a === 2) ? console.log(2) : console.log('Not true')
+
