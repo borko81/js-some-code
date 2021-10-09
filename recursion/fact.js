0ac3ae61983c183b90b6d9674935638435e3776b
borko81 <bstoilov81@gmail.com>
2021-10-09 11:14:19 +0300

    In Vue try List, event Listener, and class binding

---
+++ recursion/fact.js
@@ -0,0 +1,15 @@
+function fact(x) {
+    if (x === 0) {
+        return 1;
+    }
+    return x * fact(x - 1)
+}
+
+let array_sum = function(my_array) {
+    if (my_array.length === 1) {
+        return my_array[0];
+    }
+    return my_array.pop() + array_sum(my_array)
+}
+
+console.log(array_sum([1,2,3,4,5,6]));
